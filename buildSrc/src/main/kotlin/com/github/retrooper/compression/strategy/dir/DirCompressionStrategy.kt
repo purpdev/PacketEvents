113b4a84c2ed3619b39a8ab99729c4c9b2150277
booky10 <boooky10@gmail.com>
2025-02-27 18:24:41 +0100

    Refactor how mappings are stored
    These changes were done to split the block state mappings up into multiple files to prevent lazy-loading having to skip over the entire file for modern versions and to align mapping file names with vanilla registry names
    
    Additionally, this also splits item base components into multiple files to prepare for lazy loading them too, as they will probably also constantly grow in size with each new release

--- buildSrc/src/main/kotlin/com/github/retrooper/compression/strategy/dir/DirCompressionStrategy.kt
+++ buildSrc/src/main/kotlin/com/github/retrooper/compression/strategy/dir/DirCompressionStrategy.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of packetevents - https://github.com/retrooper/packetevents
- * Copyright (C) 2024 retrooper and contributors
+ * Copyright (C) 2025 retrooper and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,20 +15,24 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.retrooper.compression.strategy
 
+package com.github.retrooper.compression.strategy.dir
+
+import com.github.retrooper.compression.strategy.CompressionStrategy
 import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.walk
 
-interface CompressionStrategy {
+abstract class DirCompressionStrategy(
+    private val strategy: CompressionStrategy
+) : CompressionStrategy {
 
-    companion object {
-        const val COMPRESSION_VERSION: Byte = 3
-
-        fun resolveTargetPath(output: Path, relativePath: String): Path {
-            return output.resolve(relativePath.replace(".json", ".nbt"))
+    @OptIn(ExperimentalPathApi::class)
+    override fun compress(from: Path, to: Path) {
+        from.walk().forEach {
+            val relativePath = from.relativize(it).toString()
+            val target = CompressionStrategy.resolveTargetPath(to, relativePath)
+            this.strategy.compress(it, target)
         }
     }
-
-    fun compress(from: Path, to: Path)
-
 }
