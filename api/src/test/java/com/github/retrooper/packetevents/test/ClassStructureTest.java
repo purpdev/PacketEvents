fe316a4368fb1991c340a39bb1b3073ccc425e0f
booky10 <boooky10@gmail.com>
2025-02-28 19:49:27 +0100

    Small refactor to ClassStructureTest

--- api/src/test/java/com/github/retrooper/packetevents/test/ClassStructureTest.java
+++ api/src/test/java/com/github/retrooper/packetevents/test/ClassStructureTest.java
@@ -32,7 +32,8 @@ import io.github.classgraph.MethodInfoList;
 import io.github.classgraph.ScanResult;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -42,93 +43,110 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ClassStructureTest {
 
-    @Test
-    @DisplayName("Test class-structure of api")
-    public void testAll() {
-        ClassGraph graph = new ClassGraph()
+    private static ScanResult SCAN_RESULT;
+
+    @BeforeAll
+    public static void setupClassScan() {
+        SCAN_RESULT = new ClassGraph()
                 .acceptPackages(PacketEvents.class.getPackage().getName())
-                .enableAllInfo();
+                .enableAllInfo()
+                .scan();
+    }
 
-        List<String> issues = new ArrayList<>();
-        try (ScanResult scan = graph.scan()) {
-            ClassInfoList directMappedEntities = scan.getClassesImplementing(MappedEntity.class)
-                    .directOnly()
-                    .filter(c -> !c.isInterface())
-                    .filter(c -> !c.hasAnnotation(Deprecated.class));
-            if (directMappedEntities.size() > 1) {
-                issues.add("Found more than one direct MappedEntity implementation; expected only AbstractMappedEntity, found: \n"
+    @AfterAll
+    public static void tearDownClassScan() {
+        try (ScanResult scan = SCAN_RESULT) { /**/ }
+    }
+
+    @Test
+    @DisplayName("Test only one class extends MappedEntity")
+    public void testAll() {
+        ClassInfoList directMappedEntities = SCAN_RESULT.getClassesImplementing(MappedEntity.class)
+                .directOnly().filter(c -> !c.isInterface() && !c.hasAnnotation(Deprecated.class));
+        assertEquals(1, directMappedEntities.size(),
+                () -> "Found more than one direct MappedEntity implementation; expected only AbstractMappedEntity, found: \n"
                         + directMappedEntities.stream().map(ClassInfo::toStringWithSimpleNames).collect(Collectors.joining("\n")));
-            }
-            // iterate over all AbstractMappedEntity implementations
-            for (ClassInfo mappedEntity : scan.getAllClasses()
-                    .filter(info -> info.extendsSuperclass(AbstractMappedEntity.class))
-                    .filter(info -> !info.isAnonymousInnerClass())
-                    .filter(c -> !c.hasAnnotation(Deprecated.class))) {
-                String classPrefix = '[' + mappedEntity.getName() + ']';
-                MethodInfoList constructors = mappedEntity.getConstructorInfo();
-                // find data constructor
-                List<MethodInfo> dataCtors = constructors.stream()
-                        .filter(ClassMemberInfo::isPublic)
-                        .filter(info -> Arrays.stream(info.getParameterInfo())
-                                .anyMatch(param -> param.getTypeDescriptor().toString()
-                                        .contains(TypesBuilderData.class.getName())))
-                        .collect(Collectors.toList());
-                if (dataCtors.isEmpty()) {
-                    issues.add(classPrefix + " Found no constructor accepting TypesBuilderData in " + mappedEntity.toStringWithSimpleNames());
-                }
-                for (MethodInfo dataCtor : dataCtors) {
-                    // ensure constructor is marked as internal
-                    if (!dataCtor.hasAnnotation(ApiStatus.Internal.class)) {
-                        issues.add(classPrefix + " Constructor accepting TypesBuilderData isn't marked as @ApiStatus.Internal: " + dataCtor.toStringWithSimpleNames());
-                    }
-                    // ensure TypesBuilderData type is annotated as Nullable
-                    if (Arrays.stream(dataCtor.getParameterInfo())
-                            .filter(param -> param.getTypeDescriptor().toString()
-                                    .contains(TypesBuilderData.class.getName()))
-                            .noneMatch(param -> Optional.ofNullable(param.getTypeDescriptor().getTypeAnnotationInfo())
-                                    .map(annotations -> annotations.stream()
-                                            .anyMatch(annotation -> annotation.getName().contains(Nullable.class.getSimpleName())))
-                                    // the parameter type annotation disappears for non-static inner classes for some reason,
-                                    // fall back to normal parameter annotations
-                                    .orElseGet(() -> param.hasAnnotation(Nullable.class)))) {
-                        issues.add(classPrefix + " TypesBuilderData parameter type doesn't have @Nullable annotation: " + dataCtor.toStringWithSimpleNames());
-                    }
-                }
-            }
+    }
 
-            // iterate over all registry containers
-            for (ClassInfo registry : scan.getAllClasses()
-                    .filter(c -> c.getDeclaredFieldInfo().stream()
-                            .filter(ClassMemberInfo::isStatic)
-                            .anyMatch(field -> field.getTypeSignatureOrTypeDescriptor().toString()
-                                    .contains(VersionedRegistry.class.getName())))) {
-                if (!registry.hasDeclaredField("REGISTRY")) {
-                    issues.add("No REGISTRY field present on class " + registry.toStringWithSimpleNames());
+    @Test
+    @DisplayName("Test mapped entity implementations")
+    public void testMappedEntityImplementations() {
+        List<String> issues = new ArrayList<>();
+        for (ClassInfo mappedEntity : SCAN_RESULT.getAllClasses()
+                .filter(info -> info.extendsSuperclass(AbstractMappedEntity.class))
+                .filter(info -> !info.isAnonymousInnerClass())
+                .filter(c -> !c.hasAnnotation(Deprecated.class))) {
+            String classPrefix = '[' + mappedEntity.getName() + ']';
+            MethodInfoList constructors = mappedEntity.getConstructorInfo();
+            // find data constructor
+            List<MethodInfo> dataCtors = constructors.stream()
+                    .filter(ClassMemberInfo::isPublic)
+                    .filter(info -> Arrays.stream(info.getParameterInfo())
+                            .anyMatch(param -> param.getTypeDescriptor().toString()
+                                    .contains(TypesBuilderData.class.getName())))
+                    .collect(Collectors.toList());
+            if (dataCtors.isEmpty()) {
+                issues.add(classPrefix + " Found no constructor accepting TypesBuilderData in " + mappedEntity.toStringWithSimpleNames());
+            }
+            for (MethodInfo dataCtor : dataCtors) {
+                // ensure constructor is marked as internal
+                if (!dataCtor.hasAnnotation(ApiStatus.Internal.class)) {
+                    issues.add(classPrefix + " Constructor accepting TypesBuilderData isn't marked as @ApiStatus.Internal: " + dataCtor.toStringWithSimpleNames());
                 }
-                if (!registry.hasMethod("getRegistry")) {
-                    issues.add("No registry getter present on class " + registry.toStringWithSimpleNames());
-                }
-                if (!registry.isFinal()) {
-                    issues.add("Registry not marked as final: " + registry.toStringWithSimpleNames());
-                }
-                String classPrefix = '[' + registry.getName() + ']';
-                MethodInfoList publicCtorInfo = registry.getConstructorInfo()
-                        .filter(m -> !m.isPrivate());
-                if (!publicCtorInfo.isEmpty()) {
-                    issues.add(classPrefix + " Registry has public constructor: " + publicCtorInfo);
-                }
-                for (MethodInfo define : registry.getMethodInfo("define")
-                        .filter(m -> !m.isPrivate() && !m.hasAnnotation(Deprecated.class))) {
-                    if (!define.hasAnnotation(ApiStatus.Internal.class)) {
-                        issues.add(classPrefix + " Public define method not marked as internal: " + define.toStringWithSimpleNames());
-                    }
+                // ensure TypesBuilderData type is annotated as Nullable
+                if (Arrays.stream(dataCtor.getParameterInfo())
+                        .filter(param -> param.getTypeDescriptor().toString()
+                                .contains(TypesBuilderData.class.getName()))
+                        .noneMatch(param -> Optional.ofNullable(param.getTypeDescriptor().getTypeAnnotationInfo())
+                                .map(annotations -> annotations.stream()
+                                        .anyMatch(annotation -> annotation.getName().contains(Nullable.class.getSimpleName())))
+                                // the parameter type annotation disappears for non-static inner classes for some reason,
+                                // fall back to normal parameter annotations
+                                .orElseGet(() -> param.hasAnnotation(Nullable.class)))) {
+                    issues.add(classPrefix + " TypesBuilderData parameter type doesn't have @Nullable annotation: " + dataCtor.toStringWithSimpleNames());
                 }
             }
         }
+        assertEquals(0, issues.size(), () ->
+                "Found " + issues.size() + " issues: \n" + String.join("\n", issues) + "\n");
+    }
 
-        Assertions.assertEquals(0, issues.size(), () ->
+    @Test
+    @DisplayName("Test structure of registry container classes")
+    public void testRegistryContainerClasses() {
+        List<String> issues = new ArrayList<>();
+        for (ClassInfo registry : SCAN_RESULT.getAllClasses()
+                .filter(c -> c.getDeclaredFieldInfo().stream()
+                        .filter(ClassMemberInfo::isStatic)
+                        .anyMatch(field -> field.getTypeSignatureOrTypeDescriptor().toString()
+                                .contains(VersionedRegistry.class.getName())))) {
+            if (!registry.hasDeclaredField("REGISTRY")) {
+                issues.add("No REGISTRY field present on class " + registry.toStringWithSimpleNames());
+            }
+            if (!registry.hasMethod("getRegistry")) {
+                issues.add("No registry getter present on class " + registry.toStringWithSimpleNames());
+            }
+            if (!registry.isFinal()) {
+                issues.add("Registry not marked as final: " + registry.toStringWithSimpleNames());
+            }
+            String classPrefix = '[' + registry.getName() + ']';
+            MethodInfoList publicCtorInfo = registry.getConstructorInfo()
+                    .filter(m -> !m.isPrivate());
+            if (!publicCtorInfo.isEmpty()) {
+                issues.add(classPrefix + " Registry has public constructor: " + publicCtorInfo);
+            }
+            for (MethodInfo define : registry.getMethodInfo("define")
+                    .filter(m -> !m.isPrivate() && !m.hasAnnotation(Deprecated.class))) {
+                if (!define.hasAnnotation(ApiStatus.Internal.class)) {
+                    issues.add(classPrefix + " Public define method not marked as internal: " + define.toStringWithSimpleNames());
+                }
+            }
+        }
+        assertEquals(0, issues.size(), () ->
                 "Found " + issues.size() + " issues: \n" + String.join("\n", issues) + "\n");
     }
 }
